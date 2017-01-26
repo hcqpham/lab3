a8ed349cf19e80a96d65847c6e2c12425e7aa4ca
Claire Pham <hqp002@ucsd.edu>
2017-01-26 10:40:18 -0800

    Made more changes to Java.

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,10 +10,15 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Claire's Jumbotr0n Java test");
+		$("#testjs").text("There you go, breaking things again");
+		$(".jumbotron p").toggleClass("active");
+
 	});
 
 	// Add any additional listeners here
+
+
 	// example: $("#div-id").click(functionToCall);
 	$("a.thumbnail").click(projectClick);
 
@@ -22,13 +27,24 @@ function projectClick(e) { 
     e.preventDefault();
     // In an event handler, $(this) refers to      
     // the object that triggered the event      
-    $(this).css("background-color", "#7fff00");
-    var containingProject = $(this).closest(".project");
-    containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    $(this).css("background-color", "#D0DCD3");
+   var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project. When you click on this again, this will disappear.</p></div>");
+    } else {
+       description.html("<p>I was able to get the tags to disappear and reappear, but not the description of the title.</p>");
+    	$('#hide').click(function(){
+		  $('#title').hide();
+		});
+
+		$('#show').click(function(){
+		  $('#title').show();
+		});
+}
 
 
 }
-
 
 
 }
